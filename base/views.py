36788c63670632a39df7034270565b3605bf6bf8
Isaac202 <isaacbyguaracity@gmail.com>
2021-11-14 17:39:52 -0300

    Subindo atualizações

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse, HttpResponseRedirect
 from base.models import Orden, Servico, Usuarios, Empresa
 from django.views.generic import View, TemplateView, FormView, ListView, DeleteView
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, get_user_model
+from django.contrib.auth import login, logout
 
 
 
@@ -26,9 +26,6 @@ def create_user_admin(request):
                                             cpf=cpf,
                                             tipo_user = '1',
                                             password=senha,
-                                            criado_por = email,
-                                            cnpj_empresa = cnpj,
-                                            empresa_id = cnpj
                                             )
         print(user)
         user.save()
@@ -54,9 +51,6 @@ def edit_user_admin(request):
                                             cnpj=cnpj,
                                             cpf=cpf,
                                             tipo_user = '1',
-                                        
-                                            criado_por = email,
-                                            cnpj_empresa = cnpj,
                                             empresa_id = cnpj
                                             )
         u = User.objects.get(username=request.user.username)
@@ -86,7 +80,6 @@ def create_proficional(request):
                                             cpf=cpf,
                                             tipo_user = '3',
                                             password=senha,
-                                            criado_por = email,
                                             especialidade_user = especialidades,
                                             )
         print(user)
@@ -138,23 +131,59 @@ def proficional(request):
     return render(request,'admin/proficional.html')
 
 def usuario(request):
-    
+    if request.method == "POST":
+        nome = request.POST['nome']
+        email = request.POST['email']
+        cpf = request.POST['cpf']
+        cnpj = request.POST['cnpj']
+        senha = request.POST['senha']
+        user = Usuarios.objects.create_user(nome_completo=nome,
+                                            username=email,
+                                            email_user=email,
+                                            cnpj=cnpj,
+                                            cpf=cpf,
+                                            tipo_user = '4',
+                                            password=senha,
+                                            )
+        print(user)
+        user.save()
+        return JsonResponse({'status':1})
     return render(request,'admin/usuario.html')
+
+def edit_usuario(request):
+    if request.method == "POST":
+        nome = request.POST['nome']
+        email = request.POST['email']
+        cpf = request.POST['cpf']
+        cnpj = request.POST['cnpj']
+        senha = request.POST['senha']
+        user = Usuarios.objects.filter(email_user = request.user.username).update(nome_completo=nome,
+                                            username=email,
+                                            email_user=email,
+                                            cnpj=cnpj,
+                                            cpf=cpf,
+                                            tipo_user = '1',
+                                            cnpj_empresa = cnpj
+                                            )
+        u = User.objects.get(username=request.user.username)
+        u.set_password(senha)
+        u.save()
+        print(user)
+        return JsonResponse({'status':1})
+    return render(request,'admin/usuario.html')
+
+def delet_usuario(request):
+    u = User.objects.get(username=request.user.username).delete() 
+    return JsonResponse({'status':1})
 
 def especialidade(request):
     if request.method == "POST":
         user = request.user.id
-        usere = list(Usuarios.objects.filter(id=user).values('empresa_id'))
-        empresa = usere[0]['empresa_id']
         nome = request.POST['nome']
         imagem = request.POST['img']
-        empresa = ''
-        criado_por= ''
      
         servico = Servico(nome=nome,
-                       empresa=empresa,
-                       imagem=imagem,
-                       criado_por = criado_por)
+                       imagem=imagem)
         print(servico)
         servico.save()
         return JsonResponse({'status':1})
--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\isaac de oliveira\documents\g-saude\venv\scripts\python.exe
+#!C:\Users\Isaac de Oliveira\Documents\g-saude\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
